e9eefeea1a849a472e447db7068dbe7b9c044e26
Vadym Vatsenko <vadim.vatsenko@gmail.com>
2024-01-04 17:38:53 +0200

    done homework_3

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -16,12 +16,20 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
+
+            if(GetTemperature() >= overheatTemperature)
+            {
+                Debug.Log("Перегрев");
+                return;
+            }
+
+            IncreaseTemperature();
+
+            for (float i = 1.0f; i <= GetTemperature(); i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
         }
 
         protected override List<Vector2Int> SelectTargets()
