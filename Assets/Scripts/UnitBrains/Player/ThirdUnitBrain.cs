2d0eb632bb7b6e660bd7ddb8e37347f438b2a463
Vadym Vatsenko <vadim.vatsenko@gmail.com>
2024-02-14 19:11:27 +0200

    Add start sctipt for HomeWork 8

--- Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -1,13 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
-using Model.Runtime.Projectiles;
 using UnitBrains.Player;
 using UnityEngine;
-
 
 public class ThirdUnitBrain : DefaultPlayerUnitBrain
 {
     public override string TargetUnitName => "Ironclad Behemoth";
+
+    private float transitionTimer = 1f; 
+    private float currentTransitionTime = 0f; 
+
+    private bool isAttacking = false; 
+    private bool isMoving = false; 
+    public override Vector2Int GetNextStep()
+    {
+        isMoving = true;
+
+
+        return base.GetNextStep(); 
+
+
+
+      
+        
+    }
+
+   
+    protected override List<Vector2Int> SelectTargets()
+    {
+        isAttacking = true;
+
+        Debug.Log(isAttacking);
+
+        
+        return base.SelectTargets(); 
+       
+      
+    }
+
+ 
+    public override void Update(float deltaTime, float time)
+    {
+        base.Update(deltaTime, time);
+    }
 }
 
 
